d56296349bb983376787991d4fe5bb28de992a69
CarageaAlexandru <caragea.alexandru@yahoo.com>
2022-10-31 13:42:39 +0000

    plan added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>NC News</h1>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1>NC News</h1>
+			</header>
+		</div>
+	);
 }
 
 export default App;
