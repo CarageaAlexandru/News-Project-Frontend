6b5a2e650f11a9c7a851ec405d7c550370fb755e
CarageaAlexandru <caragea.alexandru@yahoo.com>
2022-11-01 01:34:28 +0000

    displays articles list - added utils function for date

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -3,6 +3,7 @@ import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
+import { getDay, getHour } from "../Utils/utils";
 
 const Articles = () => {
 	const [articles, setArticles] = useState({});
@@ -20,18 +21,20 @@ const Articles = () => {
 	if (loading) return <p>Loading...</p>;
 
 	return (
-		<div className="row row-cols-1 row-cols-md-2 g-4 gap-4">
+		<div className="mt-2 ms-2 row row-cols-1 row-cols-md-2 g-4 gap-4">
 			{articles.map((article) => {
+				const day = getDay(article.created_at);
+				const hour = getHour(article.created_at);
 				return (
-					<Card style={{ width: "18rem" }} className="text-primary">
-						<Card.Body>
+					<Card style={{ width: "18rem" }} className="text-primary ">
+						<Card.Body >
 							<Card.Title>{article.title}</Card.Title>
 							<Card.Subtitle className="mb-2 text-muted">
 								By {article.author}
 							</Card.Subtitle>
 						</Card.Body>
 						<ListGroup className="list-group-flush">
-							<ListGroup.Item>Created at: </ListGroup.Item>
+							<ListGroup.Item>Created at: {day} : {hour}</ListGroup.Item>
 							<ListGroup.Item>
 								Coment count: {article.comment_count}
 							</ListGroup.Item>
@@ -42,9 +45,11 @@ const Articles = () => {
 								Votes: {article.votes}
 							</ListGroup.Item>
 							<ListGroup.Item>
-                            <Link to={`/api/articles/${article.article_id}`}>
-								See article
-							</Link>
+								<Link
+									to={`/api/articles/${article.article_id}`}
+								>
+									See article
+								</Link>
 							</ListGroup.Item>
 						</ListGroup>
 					</Card>
